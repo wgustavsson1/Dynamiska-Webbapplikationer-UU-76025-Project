f6d226377a5f8a84bd1533abce1faa1a3d6a69fc
Wilhelm Gustavsson <wilhelm.gustavsson8992@gmail.com>
2023-01-26 18:22:38 +0100

    Added simple interface to search food function

--- js/index.js
+++ js/index.js
@@ -1,13 +1,34 @@
 import {getTranslation,getFoodData,test} from './api.js';
 
+var submit_button = document.getElementById("search-form-submit-button");
+submit_button.addEventListener("click", search);
+
+export function search()
+{
+    var search_text =  document.getElementById("input-search").value;
+    find_food(search_text)
+}
+
 function find_food(sentence)
 {
     getTranslation(sentence).then(function(translated){
         const translated_text = translated.translated_text
+
         getFoodData(translated_text).then(function(food){
-            console.log(food);
+            display_food(food);
         });
     });
 }
 
-find_food("blåbärspaj med lingonsylt och gräddfil");
+function display_food(food)
+{
+    const result = document.getElementById("search-result-ul")
+    console.log(food)
+    console.log(food)
+    food.forEach(f => {
+        const new_li = document.createElement("li");
+        new_li.innerHTML = f.name;
+        result.append(new_li)
+        console.log(f)
+    });
+}
